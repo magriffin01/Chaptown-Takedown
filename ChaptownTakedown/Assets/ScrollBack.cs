2784b056cfc74847e9dc0986d03dcf3f87090ca6
Parsa Hassani <mohamad.parsa.hasani@gmail.com>
2020-01-15 09:53:56 -0800

    0.001

---
+++ ChaptownTakedown/Assets/ScrollBack.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollBack : MonoBehaviour
+{
+    public float speed = -5f;
+    public Vector2 ground;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ground = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newPos = Mathf.Repeat(Time.time * speed, 20);
+        transform.position = ground +Vector2.right * newPos;
+    }
+}
