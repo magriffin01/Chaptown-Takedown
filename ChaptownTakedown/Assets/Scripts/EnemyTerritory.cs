959ded22e2d765b6920f6073677641864a2935e2
Sadie Jones <sadie33jones@gmail.com>
2020-01-26 17:54:23 -0800

    tilemap time bois

--- ChaptownTakedown/Assets/Scripts/EnemyTerritory.cs
+++ ChaptownTakedown/Assets/Scripts/EnemyTerritory.cs
@@ -8,7 +8,8 @@ public class EnemyTerritory : MonoBehaviour
     public Transform enemyAttackPoint;
     public Transform enemyTerritory;
     public float enemyAttackRange = 1f;
-    public LayerMask playerLayers;
+    public LayerMask player1Layers;
+    public LayerMask player2Layers;
     public int enemyAttackDamage = 10;
     public float enemyAttackDelay = 1f;
 
@@ -34,8 +35,22 @@ public class EnemyTerritory : MonoBehaviour
     void Update()
     {
         enemyAttackTimer += Time.deltaTime;
-        Collider2D[] enterTerritory = Physics2D.OverlapCircleAll(enemyTerritory.position, enemyAttackRange, playerLayers);
-        foreach (Collider2D player in enterTerritory)
+        Collider2D[] enterTerritory1 = Physics2D.OverlapCircleAll(enemyTerritory.position, enemyAttackRange, player1Layers);
+        foreach (Collider2D player in enterTerritory1)
+        {
+            if (enemyHealth.currentHealth > 0 && enemyAttackTimer >= enemyAttackDelay)
+            {
+                Attack();
+            }
+
+            else
+            {
+                Rest();
+            }
+        }
+
+        Collider2D[] enterTerritory2 = Physics2D.OverlapCircleAll(enemyTerritory.position, enemyAttackRange, player2Layers);
+        foreach (Collider2D player in enterTerritory2)
         {
             if (enemyHealth.currentHealth > 0 && enemyAttackTimer >= enemyAttackDelay)
             {
@@ -54,12 +69,18 @@ public class EnemyTerritory : MonoBehaviour
     {
 		animator.SetTrigger("enemyAttack");
         enemyAttackTimer = 0;
-        Collider2D[]hitPlayers = Physics2D.OverlapCircleAll(enemyAttackPoint.position, enemyAttackRange, playerLayers);
-        foreach(Collider2D player in hitPlayers)
+        Collider2D[]hitPlayer1 = Physics2D.OverlapCircleAll(enemyAttackPoint.position, enemyAttackRange, player1Layers);
+        foreach(Collider2D player in hitPlayer1)
         {
             player.GetComponent<Player1Health>().TakeDamage(enemyAttackDamage);
             // how do I make both players vulnerable to enemy damage independently?
             // i.e. can I make them both lose health in a way that doesn't force them both to lose health when just one is attacked?
+        }
+
+        Collider2D[]hitPlayer2 = Physics2D.OverlapCircleAll(enemyAttackPoint.position, enemyAttackRange, player2Layers);
+        foreach (Collider2D player in hitPlayer2)
+        {
+            player.GetComponent<Player2Health>().TakeDamage(enemyAttackDamage);
         }
     }
 
