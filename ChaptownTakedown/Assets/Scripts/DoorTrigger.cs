ab5d592f1f9ffbac7757e87b4d3cfeee91b70300
Mark Griffin <56698187+magriffin01@users.noreply.github.com>
2020-01-28 00:41:20 -0800

    Broken Ass Door

---
+++ ChaptownTakedown/Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    [SerializeField]
+    GameObject door;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("Touched");
+        door.transform.position = new Vector2 (0, -1);
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        door.transform.position = new Vector2 (0, -2);
+    }
+}
