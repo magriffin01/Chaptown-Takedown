7754d791a9bbae94db637296ff50285857204889
Parsa Hassani <mohamad.parsa.hasani@gmail.com>
2020-01-28 20:21:09 -0800

    main menu

---
+++ ChaptownTakedown/Assets/Scripts/OptionsOpener.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class OptionsOpener : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void OnClick()
+    {
+         Application.LoadLevel(1);
+    }
+}
