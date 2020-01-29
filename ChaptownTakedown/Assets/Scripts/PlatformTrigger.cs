015491f165016757c264d56ad2b768ffe96cfdb8
Mark Griffin <56698187+magriffin01@users.noreply.github.com>
2020-01-28 19:08:02 -0800

    Half working Platform

---
+++ ChaptownTakedown/Assets/Scripts/PlatformTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformTrigger : MonoBehaviour
+{
+    public Transform pos1, pos2;
+    public float speed;
+    public Transform startPos;
+
+    Vector3 nextPos;
+
+    [SerializeField]
+    GameObject platform;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        // platform.transform.position += new Vector3 (0, 3);
+        nextPos = pos1.position;
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        // platform.transform.position += new Vector3 (0, -3);
+        nextPos = pos2.position;
+    }
+
+    void Update()
+    {
+        platform.transform.position = Vector3.MoveTowards(platform.transform.position, nextPos, speed * Time.deltaTime);
+    }
+
+    void Start()
+    {
+        nextPos = startPos.position;
+    }
+}
